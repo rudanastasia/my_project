fa2e3b082e35ff3e8c049600f636e9ee332f5daf
rudanastasia <135504050+rudanastasia@users.noreply.github.com>
2023-08-06 22:37:41 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,3 +21,24 @@ const getVowels = stringToFilter => {
 }
 
 console.log(getVowels(string)) 
+
+//////
+const workers = [
+    {"name":"John","salary":500},
+    {"name":"Mike","salary":1300},
+    {"name":"Linda","salary":1500}
+];
+
+const getWorthyWorkers = workersArr => {
+    const WorthyWorkers = [];
+
+    workersArr.forEach(currentWorker => {
+        if (currentWorker.salary > 1000) {
+            WorthyWorkers.push(currentWorker.name)
+        }
+    })
+
+    return WorthyWorkers;
+}
+
+console.log(getWorthyWorkers(workers));
