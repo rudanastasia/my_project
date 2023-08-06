c555dfdafd295e3f3d59e5210cc0e83916de0ed9
rudanastasia <135504050+rudanastasia@users.noreply.github.com>
2023-08-07 00:57:43 +0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,46 +1,32 @@
-///////////
 const openButton = document.querySelector("#openOverlay");
+const successModal = createModal("The massage has been sent, thank you!");
 const body = document.body;
-const successModal = createModal("The message has been sent, thank you!");
 
-openButton.addEventListener("click", (e) => {
+openButton.addEventListener("click", e => {
   body.appendChild(successModal);
-});
+})
 
 function createModal(content) {
   const overlayElement = document.createElement("div");
   overlayElement.classList.add("overlay");
 
-  overlayElement.addEventListener("click", (e) => {
-    if (!e.target.classList.contains("content")) {
+  const template = document.querySelector("#overlayTemplate");
+
+  overlayElement.innerHTML = template.innerHTML;
+  overlayElement.addEventListener("click", e => {
+    if (e.target === overlayElement) {
       closeElement.click();
     }
-    // if (e.target === overlayElement) {
-    //   closeElement.click();
-    // }
-  });
+  })
 
-  const containerElement = document.createElement("div");
-  containerElement.classList.add("modal-container");
-
-  const contentElement = document.createElement("div");
-  contentElement.classList.add("content");
-
-  contentElement.innerHTML = content;
-
-  const closeElement = document.createElement("a");
-  closeElement.classList.add("close");
-  closeElement.textContent = "x";
-  closeElement.href = "#";
-
-  closeElement.addEventListener("click", (e) => {
+  const closeElement = overlayElement.querySelector(".close");
+  closeElement.addEventListener("click", e => {
     e.preventDefault();
     body.removeChild(overlayElement);
-  });
+  })
 
-  overlayElement.appendChild(containerElement);
-  containerElement.appendChild(closeElement);
-  containerElement.appendChild(contentElement);
+  const contentElement = overlayElement.querySelector(".content");
+  contentElement.innerHTML = content;
 
   return overlayElement;
 }
