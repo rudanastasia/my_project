e188e75ca87b70adff71c47d6f1b79feef5e4894
rudanastasia <135504050+rudanastasia@users.noreply.github.com>
2023-08-07 00:58:40 +0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,5 @@
 const openButton = document.querySelector("#openOverlay");
-const successModal = createModal("The massage has been sent, thank you!");
+const successModal = createModal("Hello, world!");
 const body = document.body;
 
 openButton.addEventListener("click", e => {
