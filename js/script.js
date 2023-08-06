2632bac8405b4e4d844e42d3f695403c146fb0d9
rudanastasia <135504050+rudanastasia@users.noreply.github.com>
2023-08-06 22:47:36 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -42,3 +42,16 @@ const getWorthyWorkers = workersArr => {
 }
 
 console.log(getWorthyWorkers(workers));
+
+
+////////
+const path = "/users/download/index.html"
+
+const isHtml = path => {
+    const requiredExt = ".html";
+    const pathExt = path.slice(-5);
+
+    return pathExt == requiredExt
+}
+
+console.log (isHtml(path));
