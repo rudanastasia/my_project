23408579da802faa9e0563807fee092b057f34dd
rudanastasia <135504050+rudanastasia@users.noreply.github.com>
2023-08-06 22:12:42 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
 const string = "Привет! Как дела?";
 
+const vowels = ["и", "е", "а", "е", "а"]
+
 const getVowels = stringToFilter => {
-    return stringToFilter
+    let extractedVowels = "";
+
+    for (let i = 0; i < stringToFilter.length; i++) {
+        const currentLetter = stringToFilter[i].toLowerCase();
+
+        if (vowels.includes(currentLetter)) {
+            extractedVowels += currentLetter;
+        }
+
+        
+
+    }
+
+    return extractedVowels
 
 }
 
